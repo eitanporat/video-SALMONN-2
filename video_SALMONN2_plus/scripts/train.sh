f043a5a67f6fda52f8fc1535ee18e7eb54f4d388
eporat <eporat@lightricks.com>
2025-09-17 08:06:21 +0000

    Added training script parameters scripts/train.sh

--- video_SALMONN2_plus/scripts/train.sh
+++ video_SALMONN2_plus/scripts/train.sh
@@ -1,8 +1,9 @@
 cd "$(cd $(dirname $0); pwd)/.."
 
-DATASET=preprocess_dataset/vAnnt_merge.json
-MODEL=Qwen2.5-VL-7B-Instruct
-MODEL_BASE=Qwen2.5-VL-7B-Instruct
+DATASET=/home/user/structured_captions_dataset.json
+EVAL_DATASET=/home/user/eval_structured_captions_dataset.json
+MODEL=/home/user/Video-Salmonn
+MODEL_BASE=/home/user/Video-Salmonn
 LR=2e-5
 BS=1
 ACCUM_STEPS=1
@@ -14,23 +15,30 @@ TRAIN_ENC=False
 TRAIN_AUDIO=False
 TRAIN_QFORMER=False
 EPOCH=1
-MAX_PIXELS=176400
+MAX_PIXELS=61250
 MIN_PIXELS=784
-SAVE_STEPS=1000
-MIN_FRAMES=64
-MAX_FRAMES=128
-INTERVAL=0.2
-USE_LORA=False
+SAVE_STEPS=500
+MIN_FRAMES=10
+MAX_FRAMES=256
+INTERVAL=0.1
+USE_LORA=True
 LORA_R=128
 LORA_ALPHA=256
 LORA_DROPOUT=0.05
-LORA_CKPT=No
+LORA_CKPT=tsinghua-ee/video-SALMONN-2_plus_7B
 TRAIN_TYPE=sft
-NUM_WORKER=8
+NUM_WORKER=0
 NO_AUDIO=False
+
+export CUDA_LAUNCH_BLOCKING=1
+export WANDB_API_KEY=a8ce9d9a7d252ccd3c58a35220ab899e076371f9
+export WANDB_ENTITY="lightricks"
+export WANDB_PROJECT="txt2img"
+
 
 mkdir -p output
 mkdir -p dataset
+mkdir -p eval_dataset
 
 while [[ "$#" -gt 0 ]]; do
     case $1 in
@@ -41,6 +49,7 @@ while [[ "$#" -gt 0 ]]; do
         --bs) BS="$2"; shift ;;
         --accum_steps) ACCUM_STEPS="$2"; shift ;;
         --dataset) DATASET="$2"; shift ;;
+        --eval_dataset) EVAL_DATASET="$2"; shift ;;
         --deepspeed) DEEPSPEED="$2"; shift ;;
         --train_llm) TRAIN_LLM=True ;;
         --train_proj) TRAIN_PROJ=True ;;
@@ -67,11 +76,14 @@ while [[ "$#" -gt 0 ]]; do
     shift
 done
 
-torchrun --nproc_per_node=${ARNOLD_WORKER_GPU} --nnodes="${ARNOLD_WORKER_NUM}" --node_rank="${ARNOLD_ID}" --master_addr="${METIS_WORKER_0_HOST}" --master_port=12396 \
+export MAX_STEPS=20000
+
+torchrun --nproc_per_node=4 --nnodes=1 --node_rank=0 --master_addr=127.0.0.1 --master_port=12396 \
     qwenvl/train/train_qwen.py \
         --deepspeed scripts/zero${DEEPSPEED}.json \
         --model_name_or_path "$MODEL" \
         --dataset_use $DATASET \
+        --eval_dataset_use $EVAL_DATASET \
         --tune_mm_vision $TRAIN_ENC \
         --tune_mm_mlp $TRAIN_PROJ \
         --tune_mm_llm $TRAIN_LLM \
@@ -82,15 +94,17 @@ torchrun --nproc_per_node=${ARNOLD_WORKER_GPU} --nnodes="${ARNOLD_WORKER_NUM}" -
         --gradient_accumulation_steps $ACCUM_STEPS \
         --max_pixels $MAX_PIXELS \
         --min_pixels $MIN_PIXELS \
-        --video_max_frame_pixels $MAX_PIXELS \
+        --max_steps $MAX_STEPS \
+	--video_max_frame_pixels $MAX_PIXELS \
         --video_min_frame_pixels $MIN_PIXELS \
-        --eval_strategy "no" \
+        --eval_strategy "steps" \
         --save_strategy "steps" \
         --save_steps $SAVE_STEPS \
+        --eval_steps 25 \
         --save_total_limit 5 \
         --learning_rate $LR \
         --weight_decay 0 \
-        --warmup_ratio 0.03 \
+        --warmup_steps 1000 \
         --max_grad_norm 1 \
         --lr_scheduler_type "cosine" \
         --logging_steps 1 \
